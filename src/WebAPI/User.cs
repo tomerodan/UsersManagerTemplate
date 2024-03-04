383fbcdddc35cea5125312f05b5ecb27a2a07302
tomerodan@gmail.com <tomerodan@gmail.com>
2024-03-04 15:22:54 +0100

    added User.cs

---
+++ src/WebAPI/User.cs
@@ -0,0 +1,9 @@
+namespace WebAPI;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
