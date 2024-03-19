a68457af560b25d379a4ff021b209c043ddca24d
tomerodan@gmail.com <tomerodan@gmail.com>
2024-03-19 22:58:35 +0100

    added email to user entity

--- src/MyApp.Domain/Entities/User.cs
+++ src/MyApp.Domain/Entities/User.cs
@@ -4,7 +4,8 @@ public class User
 {
     public Guid Id { get; set; }
     public string Username { get; set; }
+    public string Email { get; set;  }
+    public string PasswordHash { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string PasswordHash { get; set; }
 }
---
+++ src/MyApp.Infrastructure/DAL/Migrations/20240319215726_AddedEmailColumnToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEmailColumnToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+        }
+    }
+}
